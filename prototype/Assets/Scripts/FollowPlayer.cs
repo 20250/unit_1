7ff914fb01b90792433d3ca1a0e8240ed9912f69
20250 <125621387+20250@users.noreply.github.com>
2023-02-25 12:35:08 +1300

    trying to fix errors
    i made it worse

--- prototype/Assets/Scripts/FollowPlayer.cs
+++ prototype/Assets/Scripts/FollowPlayer.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -10);
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +16,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //placing camera behind car
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + new Vector3(0, 5, -7);
     }
 }
